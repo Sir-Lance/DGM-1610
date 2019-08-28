bbb9d2f7ef2fe11325449a11d00b6aace268d42d
Lance <lanceapeters@gmail.com>
2019-08-28 17:01:26 -0600

    Scripts Updated, GameObject modified

--- 2D_Platformer/Assets/Scripts/Variables.cs
+++ 2D_Platformer/Assets/Scripts/Variables.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    string name = "Lance";
+    string firstName = "Lance";
+    int age = 21;
+    float height = 6.1f;
+    bool married = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(name);
+        Debug.Log("First Name: " + firstName);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married? " + married);
+
     }
 }
+//HOMEWORK
+//Variables for Superhero, Villain, Cartoon Character, Free
