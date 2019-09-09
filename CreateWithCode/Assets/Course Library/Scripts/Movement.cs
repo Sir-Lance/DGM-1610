b3426b1b6aa74b776216883206bc678a641ccfdc
Lance <lanceapeters@gmail.com>
2019-09-09 17:01:42 -0600

    3d Project Updated

---
+++ CreateWithCode/Assets/Course Library/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves vehicle along z axis
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
