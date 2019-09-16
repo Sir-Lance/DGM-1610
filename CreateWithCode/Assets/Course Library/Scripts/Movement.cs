4abf068eaf6e5bbed33c8e5a34fa3b86c9c61f89
Lance <lanceapeters@gmail.com>
2019-09-16 17:06:54 -0600

    Challenge completed

--- CreateWithCode/Assets/Course Library/Scripts/Movement.cs
+++ CreateWithCode/Assets/Course Library/Scripts/Movement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,14 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //moves vehicle along z axis
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Moves car forward/back 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //MOves car left/right
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //no longer used
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        
     }
 }
