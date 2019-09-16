fd854c3ce50673772a6d497fefd4cc370d119540
Lance <lanceapeters@gmail.com>
2019-09-16 17:54:55 -0600

    Vehicle Rig

---
+++ Vehicle Rig/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 3, 3);
+    private Vector3 rotateVal = new Vector3(0, -90, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = player.transform.rotation + rotateVal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+
+        
+    }
+}
