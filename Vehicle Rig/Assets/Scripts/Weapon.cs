61c7ffb08f704d0a54d3aeeb408eab31b05c11d1
Lance <lanceapeters@gmail.com>
2019-09-19 14:21:20 -0600

    VehicleRig Updated

---
+++ Vehicle Rig/Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Weapon : MonoBehaviour
+// {
+//     public GameObject projectile;
+//     // Start is called before the first frame update
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+//         bullet.getCompnent<Rigidbody>().addForce(transfrom.forward * 10);
+//     }
+// }
