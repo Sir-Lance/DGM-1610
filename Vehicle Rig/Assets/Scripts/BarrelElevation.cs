4fda792b81be07927cdf9e56e0caa156d9be36ad
Lance <lanceapeters@gmail.com>
2019-10-09 17:20:58 -0600

    VehicleRig updated

---
+++ Vehicle Rig/Assets/Scripts/BarrelElevation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelElevation : MonoBehaviour
+{
+    public GameObject rotPoint;
+    public float rotSpeed = 30.0f;
+    public float verticalInput;
+    public float maxElevation = -15.0f;
+    public float maxDepression = 6.0f;
+
+    // Update is called once per frame
+       void Update()
+    {
+        //Takes mouse input to rotate barrel on Y axis
+        verticalInput = Input.GetAxisRaw("Mouse Y");
+        transform.Rotate(Vector3.left, rotSpeed * verticalInput * Time.deltaTime);
+        
+        //If statements prevent the gun from going past a certain depression or elevation
+        // if(transform.Rotate.x < maxElevation){
+        //     transform.rotation(maxElevation, 0, 0);
+        // }
+
+        //Locks cursor to the center of the screen
+        
+        if (Input.GetKey(KeyCode.Escape))
+        Screen.lockCursor = false;
+        else
+        Screen.lockCursor = true;    
+    }
+}
