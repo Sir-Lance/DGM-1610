a6e61cd5f7bee0ef67f23168413c3c7aac53d013
Lance <lanceapeters@gmail.com>
2019-10-10 19:32:11 -0600

    scriptos updato

--- Vehicle Rig/Assets/Scripts/BarrelElevation.cs
+++ Vehicle Rig/Assets/Scripts/BarrelElevation.cs
@@ -10,27 +10,46 @@ public class BarrelElevation : MonoBehaviour
     public float maxElevation = -8.0f;
     public float maxDepression = 6.0f;
 
-
     // Update is called once per frame
-       void Update()
+    void Update()
     {
+        float rotX = transform.localRotation.eulerAngles.x;
+        float rotY = transform.localRotation.eulerAngles.y;
+        float rotZ = transform.localRotation.eulerAngles.z;
+        
         //Takes mouse input to rotate barrel on Y axis
         verticalInput = Input.GetAxisRaw("Mouse Y");
         transform.Rotate(Vector3.left, rotSpeed * verticalInput * Time.deltaTime);
         
         //If statements prevent the gun from going past a certain depression or elevation
-        //NOT FUCKING WORKING
-        if(transform.rotation.z <= maxElevation)
+        
+        if(rotX > 180f)
         {
-            transform.rotation = new Quaternion(maxElevation, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            rotX -= 360f;
+        }
+
+        if(rotX < -180f)
+        {
+            rotX += 360f;
+        }
+        
+        if(rotX < maxElevation)
+        {
+            Vector3 newRot = new Vector3(maxElevation, rotY, rotZ);
+            var angle = Quaternion.Euler(newRot);
+            transform.localRotation = angle;
             Debug.Log("High");
         }
-        if(transform.rotation.z >= maxDepression)
+        
+        if(rotX > maxDepression)
         {
-            transform.rotation = new Quaternion(-maxDepression, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            Vector3 newRot = new Vector3(maxDepression, rotY, rotZ);
+            var angle = Quaternion.Euler(newRot);
+            transform.localRotation = angle;
             Debug.Log("Low");
         }
         
+        //Debug.Log(rotX);
 
         //Locks cursor to the center of the screen
         
