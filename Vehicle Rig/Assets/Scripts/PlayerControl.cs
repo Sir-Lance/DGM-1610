d6de0a1f2f1cdbf8ba8ff3e114b4a016e752b1b3
Lance <lanceapeters@gmail.com>
2019-10-21 17:28:01 -0600

    NEWPREFABS

--- Vehicle Rig/Assets/Scripts/PlayerControl.cs
+++ Vehicle Rig/Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,19 @@ public class PlayerControl : MonoBehaviour
     public float turnSpeed = 90.0f;
     public float horizontalInput;
     public float forwardInput;
+    
+    //right wheel colliders
+    public GameObject w1r;
+    public GameObject w2r;
+    public GameObject w3r;
+    public GameObject w4r;
+    public GameObject w5r;
+    //left wheel colliders
+    public GameObject w1l;
+    public GameObject w2l;
+    public GameObject w3l;
+    public GameObject w4l;
+    public GameObject w5l;
 
     // Update is called once per frame
     void Update()
@@ -15,9 +28,17 @@ public class PlayerControl : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
         
+        // if(forwardInput == 1){
+        //     w1rC = GetComponent<WheelCollider>();
+        //     w1rC.motorTorque = 10000;
+        // }
+        
+        
+        //Legacy
         //Moves tank forward/back 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-       
+        
+        //Legacy
         //Moves tank left/right
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         
